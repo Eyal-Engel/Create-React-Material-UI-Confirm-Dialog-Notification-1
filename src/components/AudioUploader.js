b32be6a6bb62cb6cfb865ce3457721a497eaf8ae
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2023-01-11 11:04:50 +0200

    finish backend of file uploader

--- src/components/AudioUploader.js
+++ src/components/AudioUploader.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { useDropzone } from "react-dropzone";
-import './AudioUploader.css';
+import "./AudioUploader.css"
 
+
+const MAX_FILE_SIZE = 1000000; // 1MB in bytes
+const MAX_FILES = 5;
 
 const AudioUploader = () => {
   const [files, setFiles] = useState([]);
@@ -11,47 +14,84 @@ const AudioUploader = () => {
     const filteredFiles = acceptedFiles.filter(file => file.type.startsWith("audio/"));
     if (filteredFiles.length === 0) {
         alert("Only audio files are accepted!")
+    } else if(filteredFiles.length > MAX_FILES){
+        alert("Cannot upload more than 5 files")
     } else {
-        setFiles(filteredFiles);
+    // check file size
+      const filesUnderMaxSize = filteredFiles.filter(file => file.size <= MAX_FILE_SIZE)
+      if (filesUnderMaxSize.length === 0) {
+        alert("No files above 1MB size!");
+      } else {
+        setFiles(filesUnderMaxSize);
+      }
     }
   };
 
-  const { getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    accept: "audio/*"
-  });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept: "audio/*" });
 
-  
   const clearFiles = e => {
     e.preventDefault();
     setFiles([]);
   };
 
-  const filePreview = files.map((file, index) => {
-    return (
-      <div key={index} style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-      <audio key={index} controls src={URL.createObjectURL(file)} style={{width: "200px"}} />
-      <button onClick={clearFiles} style={{width: "25px", height: "25px"}}>X</button>
-      </div>
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+  }), [isDragActive]);
 
+  const filePreview = files.length > 0 && files.map((file, index) => {
+    return (
+      <div key={index} style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <audio src={URL.createObjectURL(file)} controls />
+        <button className="AudioXBtn" onClick={clearFiles} style={{alignSelf: "center"}}>X</button>
+      </div>
     );
   });
 
   return (
-    <div style={{ border: "1px solid grey", padding: "20px", backgroundColor: "none", width: "550px"  }}>
-      <input {...getInputProps()} id="fileInputAudio" style={{display: "none"}} />
-      {isDragActive ? (
-        <h5>Drop the audio  here ...</h5>
-      ) : (
-        <div className="textAreaDragNDrop" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-          <p>Drag and drop an audio here or </p>
-          <p><a onClick={() => {document.getElementById("fileInputAudio").click()}}>Click here</a></p>
-          <p>to select a file from file explorer</p>
-        </div>
-      )}
-      <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>{filePreview}</div>
+    <div style={{ border: "1px solid grey", padding: "20px", backgroundColor: "none", width: "550Px"}}>
+      <div className="textAreaDragNDrop" style={{
+      display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+      <p>To select an audio from file explorer </p>
+      <p><a onClick={() => {document.getElementById("fileInputAudio").click()}}>Click Here</a></p>
+      <p>or drag a file to below </p>
     </div>
-  );
+    <div {...getRootProps({style})}>
+      <input {...getInputProps()} id="fileInputAudio" style={{display: "none"}} />
+      <div style={{    display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+        {isDragActive ? 
+          <p>Drop the audio here ...</p>
+        :
+          <p>Drop File Here</p>
+        }
+      </div>
+    </div>
+    <div style={{    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    }}>{filePreview}</div>
+  </div>
+);
+};
+
+const baseStyle = {
+  width: "100%",
+  height: "100%",
+  borderWidth: 2,
+  borderColor: "#666",
+  borderStyle: "dotted",
+  borderRadius: 5,
+  padding: "25px",
+  textAlign: "center",
+  marginTop: "10px"
+};
+const activeStyle = {
+  borderStyle: "solid",
+  borderColor: "#6c6",
+  backgroundColor: "#eee"
 };
 
 export default AudioUploader;
--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { useDropzone } from "react-dropzone";
+import "./ImageUploader.css"
+
+const MAX_FILE_SIZE = 1000000; // 1MB in bytes
+const MAX_FILES = 1;
 
 const ImageUploader = () => {
   const [files, setFiles] = useState([]);
@@ -9,48 +13,83 @@ const ImageUploader = () => {
     const filteredFiles = acceptedFiles.filter(file => file.type.startsWith("image/"));
     if (filteredFiles.length === 0) {
         alert("Only image files are accepted!")
+    } else if(filteredFiles.length > MAX_FILES){
+        alert("Cannot upload more than 1 file")
     } else {
-        setFiles(filteredFiles);
+    // check file size
+      const filesUnderMaxSize = filteredFiles.filter(file => file.size <= MAX_FILE_SIZE)
+      if (filesUnderMaxSize.length === 0) {
+        alert("No files above 1MB size!");
+      } else {
+        setFiles(filesUnderMaxSize);
+      }
     }
   };
 
-  const { getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    accept: "image/*"
-  });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept: "image/*" });
 
   const clearFiles = e => {
     e.preventDefault();
     setFiles([]);
   };
 
-  const filePreview = files.map((file, index) => {
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+  }), [isDragActive]);
+
+  const filePreview = files.length > 0 && files.map((file, index) => {
     return (
-      <div key={index}>
+      <div key={index} style={{marginTop: "10px"}}>
         <img src={URL.createObjectURL(file)} style={{width: "200px"}} />
-        <button onClick={clearFiles}>X</button>
+        <button className="ImageXBtn" onClick={clearFiles} >X</button>
       </div>
     );
   });
 
   return (
     <div style={{ border: "1px solid grey", padding: "20px", backgroundColor: "none", width: "550Px"  }}>
-      <input {...getInputProps()} id="fileInputImage" style={{display: "none"}} />
-      {isDragActive ? (
-        <p>Drop the image  here ...</p>
-      ) : (
-        <div className="textAreaDragNDrop" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-          <p>Drag and drop an image here or </p>
-          <p><a onClick={() => {document.getElementById("fileInputImage").click()}}>Click here</a></p>
-          <p>to select a file from file explorer</p>
+      <div className="textAreaDragNDrop" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+        <p>To select an image from file explorer </p>
+        <p><a onClick={() => {document.getElementById("fileInputImage").click()}}>Click Here</a></p>
+        <p>or drag a file to below </p>
+      </div>
+      <div {...getRootProps({style})}>
+        <input {...getInputProps()} id="fileInputImage" style={{display: "none"}} />
+        <div style={{    display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+          {isDragActive ? 
+            <p>Drop the image here ...</p>
+          :
+            <p>Drop File Here</p>
+          }
         </div>
-      )}
+      </div>
       <div style={{    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    }}>{filePreview}</div>
+      alignItems: "center",
+      justifyContent: "center",
+      }}>{filePreview}</div>
     </div>
   );
+};
+
+const baseStyle = {
+    width: "100%",
+    height: "100%",
+    borderWidth: 2,
+    borderColor: "#666",
+    borderStyle: "dotted",
+    borderRadius: 5,
+    padding: "25px",
+    textAlign: "center",
+    marginTop: "10px"
+};
+const activeStyle = {
+    borderStyle: "solid",
+    borderColor: "#6c6",
+    backgroundColor: "#eee"
 };
 
 export default ImageUploader;
